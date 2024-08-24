cc7e660e70b1d2241e816abc4602261b06a5369e
Ojasvi <ojasvidoye@gmail.com>
2024-08-24 18:51:10 +0530

    add asyncHandler utility

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,122 +2,103 @@ import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
 dotenv.config();
 
-export const register = async (req, res) => {
+export const register = asyncHandler(async (req, res) => {
   const { username, password } = req.body;
-  try {
-    const checkUsername = await User.findOne({ username });
-    if (checkUsername) {
-      return res.status(409).json({ msg: "username already exists" }); 
-    }
-    const saltRounds = 10;
-    const salt = await bcrypt.genSalt(saltRounds);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    const user = await User.create({
-      username,
-      password: hashedPassword,
-    });
-
-    res.status(201).json(user);
-  } catch (err) {
-    res.status(500).json({ msg: err });
+  const checkUsername = await User.findOne({ username });
+  if (checkUsername) {
+    return res.status(409).json({ msg: "username already exists" });
   }
-};
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-export async function login(req, res) {
+  const user = await User.create({
+    username,
+    password: hashedPassword,
+  });
+
+  res.status(201).json(user);
+});
+
+export const login = asyncHandler(async (req, res) => {
   const { username, password } = req.body;
 
-  try {
-    const user = await User.findOne({ username });
-    if (!user) return res.status(404).json({ msg: "user not found" }); 
+  const user = await User.findOne({ username });
+  if (!user) return res.status(404).json({ msg: "user not found" });
 
-    const isPasswordValid = bcrypt.compareSync(password, user.password);
-    if (!isPasswordValid)
-      return res.status(401).json({ msg: "invalid password" }); 
+  const isPasswordValid = bcrypt.compareSync(password, user.password);
+  if (!isPasswordValid)
+    return res.status(401).json({ msg: "invalid password" });
 
-    const payload = { username, id: user._id };
+  const payload = { username, id: user._id };
 
-    const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
-      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+  const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+  });
+
+  const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+  });
+
+  user.refreshToken = refreshToken;
+  await user.save();
+
+  res
+    .cookie("access_token", accessToken, {
+      httpOnly: true,
+    })
+    .cookie("refresh_token", refreshToken, {
+      httpOnly: true,
     });
 
-    const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
-      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
-    });
+  return res.status(200).json({ msg: "login successful" });
+});
 
-    user.refreshToken = refreshToken;
-    await user.save();
-
-    res
-      .cookie("access_token", accessToken, {
-        httpOnly: true,
-      })
-      .cookie("refresh_token", refreshToken, {
-        httpOnly: true,
-      });
-
-    return res.status(200).json({ msg: "login successful" });
-  } catch (error) {
-    return res.status(500).json({ msg: error });
-  }
-}
-
-export async function refreshToken(req, res) {
+export const refreshToken = asyncHandler(async (req, res) => {
   const { refresh_token } = req.cookies;
 
   if (!refresh_token) {
-    return res.status(400).json({ msg: "refresh token doesn't exist" }); 
+    return res.status(400).json({ msg: "refresh token doesn't exist" });
   }
-  try {
-    const token = jwt.verify(refresh_token, process.env.REFRESH_TOKEN_SECRET);
-    const user = await User.findOne({ username: token.username }); 
+  const token = jwt.verify(refresh_token, process.env.REFRESH_TOKEN_SECRET);
+  const user = await User.findOne({ username: token.username });
 
-    if (!user || user.refreshToken !== refresh_token) {
-      return res.status(403).json({ msg: "invalid refresh token" });
-    }
+  if (!user || user.refreshToken !== refresh_token) {
+    return res.status(403).json({ msg: "invalid refresh token" });
+  }
 
-    const payload = { username: token.username, id: token.id };
+  const payload = { username: token.username, id: token.id };
 
-    const newAccessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
-      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+  const newAccessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+  });
+
+  const newRefreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+  });
+
+  user.refreshToken = newRefreshToken;
+  await user.save();
+
+  res
+    .cookie("access_token", newAccessToken, {
+      httpOnly: true,
+    })
+    .cookie("refresh_token", newRefreshToken, {
+      httpOnly: true,
     });
 
-    const newRefreshToken = jwt.sign(
-      payload,
-      process.env.REFRESH_TOKEN_SECRET,
-      {
-        expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
-      }
-    );
+  return res.status(200).json({ msg: "tokens refreshed successfully woohoo!" });
+});
 
-    user.refreshToken = newRefreshToken;
-    await user.save();
-
-    res
-      .cookie("access_token", newAccessToken, {
-        httpOnly: true,
-      })
-      .cookie("refresh_token", newRefreshToken, {
-        httpOnly: true,
-      });
-
-    return res
-      .status(200)
-      .json({ msg: "tokens refreshed successfully woohoo!" });
-  } catch (error) {
-    return res.status(403).json({ msg: error });
-  }
-}
-
-export const logout = async (req, res) => {
-  try {
-    res
-      .cookie("access_token", "", { httpOnly: true })
-      .cookie("refresh_token", "", { httpOnly: true })
-      .status(200).json({ msg: "logout successful" }); 
-  } catch (error) {
-    return res.status(500).json({ msg: error }); 
-  }
-};
+export const logout = asyncHandler(async (req, res) => {
+  res
+    .cookie("access_token", "", { httpOnly: true })
+    .cookie("refresh_token", "", { httpOnly: true })
+    .status(200)
+    .json({ msg: "logout successful" });
+});
--- server/controllers/transaction.controller.js
+++ server/controllers/transaction.controller.js
@@ -1,76 +1,59 @@
 import { Transaction } from "../models/transaction.model.js";
 import { User } from "../models/user.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
-export const addTransaction = async (req, res) => {
+export const addTransaction = asyncHandler(async (req, res) => {
   const { title, amount, category, description, userId, transactionType } =
     req.body;
-  try {
-    if (!title || !amount || !description || !category || !transactionType) {
-      return res.status(400).json({ msg: "fill all fields" });
-    }
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(404).json({ msg: "user doesn't exist" });
-    }
-
-    const response = await Transaction.create({
-      title: title,
-      amount: amount,
-      category: category,
-      description: description,
-      user: userId,
-      transactionType: transactionType,
-    });
-    return res.status(200).json({ msg: "transaction added successfully" });
-  } catch (error) {
-    return res.status(500).json({ msg: error });
+  if (!title || !amount || !description || !category || !transactionType) {
+    return res.status(400).json({ msg: "fill all fields" });
   }
-};
-
-export const showAllTransactions = async (req, res) => {
-  try {
-    const transactions = await Transaction.find({});
-    return res.status(200).json({ msg: transactions });
-  } catch (error) {
-    return res.status(500).json({ msg: error });
+  const user = await User.findById(userId);
+  if (!user) {
+    return res.status(404).json({ msg: "user doesn't exist" });
   }
-};
 
-export const deleteTransaction = async (req, res) => {
-  try {
-    const transactionId = req.params.id;
-    const transactionElement =
-      await Transaction.findByIdAndDelete(transactionId);
-    if (!transactionElement) {
-      return res.status(404).json({ msg: "transaction not found" });
-    }
-    res.status(200).json({ msg: "deleted successfully" });
-  } catch (error) {
-    return res.status(500).json({ msg: error });
+  await Transaction.create({
+    title,
+    amount,
+    category,
+    description,
+    user: userId,
+    transactionType,
+  });
+  return res.status(200).json({ msg: "transaction added successfully" });
+});
+
+export const showAllTransactions = asyncHandler(async (req, res) => {
+  const transactions = await Transaction.find({});
+  return res.status(200).json({ msg: transactions });
+});
+
+export const deleteTransaction = asyncHandler(async (req, res) => {
+  const transactionId = req.params.id;
+  const transactionElement = await Transaction.findByIdAndDelete(transactionId);
+  if (!transactionElement) {
+    return res.status(404).json({ msg: "transaction not found" });
   }
-};
+  res.status(200).json({ msg: "deleted successfully" });
+});
 
-export const updateTransaction = async (req, res) => {
-  try {
-    const transactionId = req.params.id;
-    const { title, amount, category, description, userId, transactionType } =
-      req.body;
+export const updateTransaction = asyncHandler(async (req, res) => {
+  const transactionId = req.params.id;
+  const { title, amount, category, description, transactionType } = req.body;
 
-    const transaction = await Transaction.findById(transactionId);
-    if (!transaction) {
-      return res.status(404).json({ msg: "transaction not found" });
-    }
-
-    transaction.title = title;
-    transaction.amount = amount;
-    transaction.category = category;
-    transaction.description = description;
-    transaction.transactionType = transactionType;
-
-    await transaction.save();
-
-    res.status(200).json(transaction);
-  } catch (error) {
-    return res.status(500).json({ msg: error });
+  const transaction = await Transaction.findById(transactionId);
+  if (!transaction) {
+    return res.status(404).json({ msg: "transaction not found" });
   }
-};
+
+  transaction.title = title;
+  transaction.amount = amount;
+  transaction.category = category;
+  transaction.description = description;
+  transaction.transactionType = transactionType;
+
+  await transaction.save();
+
+  res.status(200).json(transaction);
+});
