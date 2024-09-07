4248ef943f1ddd7a5983143884f64f0104dd3fc1
Ojasvi <ojasvidoye@gmail.com>
2024-09-07 21:34:49 +0530

    add registration page

--- client/src/Pages/RegisterPage.jsx
+++ client/src/Pages/RegisterPage.jsx
@@ -1,7 +1,50 @@
-const RegisterPage = () => {
-  return (
-    <div>RegisterPage</div>
-  )
-}
+import axios from "axios";
+import { useState } from "react";
 
-export default RegisterPage
+const RegisterPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/v1/register",
+        { username, password },
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+
+      if (response.status === 201) {
+        alert("registration successful");
+      }
+    } catch (error) {
+      alert("error");
+      console.log("error:", error);
+    }
+  };
+  return (
+    <div>
+      <h2>Registration</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={username}
+          placeholder="username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterPage;
